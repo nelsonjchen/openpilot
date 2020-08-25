92d5a918951ab081717270c071471f3727bfaee6
Nelson Chen <crazysim@gmail.com>
2020-08-25 08:19:36 -0700

    restore original here; not a concern of the API

--- selfdrive/car/toyota/toyotacan.py
+++ selfdrive/car/toyota/toyotacan.py
@@ -34,7 +34,7 @@ def create_accel_command(packer, accel, pcm_cancel, standstill_req, lead):
     "ACCEL_CMD": accel,
     "SET_ME_X01": 1,
     "DISTANCE": 0,
-    "MINI_CAR": lead,
+    "MINI_CAR": 1,
     "SET_ME_X3": 3,
     "PERMIT_BRAKING": lead,
     "RELEASE_STANDSTILL": not standstill_req,
