c48c6eb51400a034a0baa722d775b9135bf4c661
Keyvan Fatehi <keyvanfatehi@gmail.com>
2021-11-24 23:57:32 -0800

    a basic interactivity

--- selfdrive/ui/qt/widgets/seethrucameraview.cc
+++ selfdrive/ui/qt/widgets/seethrucameraview.cc
@@ -1,8 +1,14 @@
 #include "selfdrive/ui/qt/widgets/seethrucameraview.h"
-
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonValue>
 #include <QDebug>
+#include "selfdrive/common/params.h"
+#include "selfdrive/common/util.h"
+#include "selfdrive/ui/qt/util.h"
 
-SeethruCameraViewWidget::SeethruCameraViewWidget(QWidget* parent) : sm({"driverState"}), CameraViewWidget(VISION_STREAM_RGB_BACK, true, parent) {
+SeethruCameraViewWidget::SeethruCameraViewWidget(QWidget* parent) : sm({"driverState"}), CameraViewWidget(VISION_STREAM_RGB_WIDE, true, parent) {
     // start a timer to poll for driverstate
     QTimer *t = new QTimer(this);
     connect(t, &QTimer::timeout, this, &SeethruCameraViewWidget::handleDriverState);
@@ -19,14 +25,85 @@ void SeethruCameraViewWidget::handleDriverState() {
         auto fxy_list = driver_state.getFacePosition();
         face_x = fxy_list[0];
         face_y = fxy_list[1];
-        // qDebug() << "face detected at x:" << face_x << " y: " << face_y;
+        qDebug() << "face detected at x:" << face_x << " y: " << face_y;
     } else {
         // qDebug() << "no face detected";
-        
+        face_x = 0;
+        face_y = 0;
     }
+    adjustTransform();
 }
 
-void SeethruCameraViewWidget::resizeGL(int w, int h) {
-    qDebug() << "resizeGL override" << w << h << face_x << face_y;
-    updateFrameMat((int)(w/face_x), (int)(h/face_y));
+void SeethruCameraViewWidget::adjustTransform() {
+    mat4 device_transform = {{
+        1.0,  0.0, 0.0, 0.0,
+        0.0,  1.0, 0.0, 0.0,
+        0.0,  0.0, 1.0, 0.0,
+        0.0,  0.0, 0.0, 1.0,
+    }};
+
+#ifdef LEARNING_TRANSFORM
+
+    QString transform_json = QString::fromStdString(util::read_file("/dev/shm/transform.json"));
+    /* To learn more about this transform, we can create the file as follows:
+    {
+        "transform": [
+            1.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.0, 0, 0.0, 1.0
+        ]
+    }
+    */
+
+    QJsonDocument json_d = QJsonDocument::fromJson(transform_json.toUtf8());
+    QJsonArray json_a = json_d["transform"].toArray();
+
+    mat4 frame_transform = {{
+        (float)json_a.at(0).toDouble(),
+        (float)json_a.at(1).toDouble(),
+        (float)json_a.at(2).toDouble(),
+        (float)json_a.at(3).toDouble(),
+        (float)json_a.at(4).toDouble(),
+        (float)json_a.at(5).toDouble(),
+        (float)json_a.at(6).toDouble(),
+        (float)json_a.at(7).toDouble(),
+        (float)json_a.at(8).toDouble(),
+        (float)json_a.at(9).toDouble(),
+        (float)json_a.at(10).toDouble(),
+        (float)json_a.at(11).toDouble(),
+        (float)json_a.at(12).toDouble(),
+        (float)json_a.at(13).toDouble(),
+        (float)json_a.at(14).toDouble(),
+        (float)json_a.at(15).toDouble()
+    }};
+    /*
+    And we learn that the positions in the matrix do the following:
+                                                                        
+    [                                                                       
+    stretchX(in,1,out),warp(top-left,0,top-right),0,translateY(left,0,right)
+    rotate(left-up,0,left-down),stretchY(in,1,out),0,translateX(down,0,up)  
+    0,0,0,0                                                                 
+    tiltX(left-out,0,right-out),tiltY(top-in,0,top-out),?,zoom(in,1,out)    
+    ]
+
+    The question still remains: what transformations should be made based on the face position?
+
+    The paper here https://dan.andersen.name/files/Andersen-ISMAR-2016.pdf uses depth information both
+    on the driver/user and the environment. We don't have depth info here (although we could probably get it?)
+    can we still achieve the illusion to some extent w/ only face position?
+    */
+#else
+    float zoom = 0.3;
+    float translateY = 0.0 + face_y;
+    float translateX = 0.0 + face_x;
+    
+    mat4 frame_transform = {{
+        1.0,  0.0, 0.0, translateY,
+        0.0,  1.0, 0.0, translateX,
+        0.0,  0.0, 1.0, 0.0,
+        0.0,  0.0, 0.0, zoom,
+    }};
+#endif
+    frame_mat = matmul(device_transform, frame_transform);
 }
--- selfdrive/ui/qt/widgets/seethrucameraview.h
+++ selfdrive/ui/qt/widgets/seethrucameraview.h
@@ -6,25 +6,10 @@ class SeethruCameraViewWidget : public CameraViewWidget {
   Q_OBJECT
 
 public:
-//   using QOpenGLWidget::QOpenGLWidget;
   explicit SeethruCameraViewWidget(QWidget* parent = nullptr);
-//   ~SeethruCameraViewWidget();
-//   void setStreamType(VisionStreamType type);
-//   void setBackgroundColor(QColor color);
-
-// signals:
-//   void clicked();
-//   void frameUpdated();
 
 protected:
-//   void paintGL() override;
-  void resizeGL(int w, int h) override;
-//   void initializeGL() override;
-//   void hideEvent(QHideEvent *event) override;
-//   void mouseReleaseEvent(QMouseEvent *event) override;
-//   void updateFrameMat(int w, int h);
-//   std::unique_ptr<VisionIpcClient> vipc_client;
-
+  void adjustTransform();
 protected slots:
   void handleDriverState();
 
@@ -33,14 +18,4 @@ private:
   float face_x;
   float face_y;
   bool face_detected;
-
-//   bool zoomed_view;
-//   VisionBuf *latest_frame = nullptr;
-//   GLuint frame_vao, frame_vbo, frame_ibo;
-//   mat4 frame_mat;
-//   std::unique_ptr<EGLImageTexture> texture[UI_BUF_COUNT];
-//   QOpenGLShaderProgram *program;
-
-//   VisionStreamType stream_type;
-//   QColor bg = QColor("#000000");
 };
