8ba4a7b562c93aaa4ad7c66727d92e87fd7ebedf
paithon-dev <francesco.messina@paithon.it>
2024-08-02 12:50:02 +0200

    Updated fingerprints.py and values.py to European Yaris 2020 TSS2.5

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -155,6 +155,7 @@ class CAR(Platforms):
     [ToyotaCarDocs("Toyota Corolla 2017-19")],
     CarSpecs(mass=2860. * CV.LB_TO_KG, wheelbase=2.7, steerRatio=18.27, tireStiffnessFactor=0.444),
     dbc_dict('toyota_new_mc_pt_generated', 'toyota_adas'),
+    pt_dbc, radar_dbc
   )
   # LSS2 Lexus UX Hybrid is same as a TSS2 Corolla Hybrid
   TOYOTA_COROLLA_TSS2 = ToyotaTSS2PlatformConfig(
@@ -264,6 +265,12 @@ class CAR(Platforms):
     CarSpecs(mass=4590. * CV.LB_TO_KG, wheelbase=3.03, steerRatio=15.5, tireStiffnessFactor=0.444),
     dbc_dict('toyota_tnga_k_pt_generated', 'toyota_adas'),
     flags=ToyotaFlags.NO_STOP_TIMER,
+  )
+  TOYOTA_YARIS_TSS2_2020 = PlatformConfig(
+    [ToyotaCarDocs("Toyota Yaris 2020-EU")],
+    CarSpecs(mass=1836. * CV.LB_TO_KG, wheelbase=2.70, steerRatio=13.0, tireStiffnessFactor=1.0),
+    dbc_dict('toyota_rav4_prime_generated', 'toyota_tss2_adas'),
+    flags=ToyotaFlags.TSS2 | ToyotaFlags.NO_STOP_TIMER | ToyotaFlags.NO_DSU | ToyotaFlags.SECOC | ToyotaFlags.ALT_GAS_MSG | ToyotaFlags.GEAR_PACKET_HYBRID,
   )
 
   # Lexus
